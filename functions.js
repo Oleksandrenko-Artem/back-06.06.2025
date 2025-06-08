9054266c91b49516752367f9188625b9e0e5cb1c
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-06-08 17:34:31 +0300

    Fix createLogFileWithDate and add getListFiles

--- functions.js
+++ functions.js
@@ -1,6 +1,19 @@
 const path = require('path');
 const { readFileAsync } = require('./utils/readFile');
 const { writeFileAsync, addFileAsync } = require('./utils/writeFile');
+const { readDirAsync } = require('./utils/readDir');
+const { createFolderAsync } = require('./utils/createFolder');
+const { fileExists } = require('./utils/existsFile');
+
+exports.getListFiles = async (dirName) => {
+    try {
+        const pathDir = path.resolve(dirName);
+        const listFiles = await readDirAsync(pathDir);
+        console.log(listFiles);
+    } catch (error) {
+        console.log("Error --->>> ", error);
+    }
+};
 
 exports.createLogFileWithDate = async (dirName) => {
     try {
@@ -9,9 +22,13 @@ exports.createLogFileWithDate = async (dirName) => {
         const dateFormat = date.replaceAll(':', '-')
         const pathFile = 'log_' + dateFormat + '.txt';
         const pathFileFull = path.resolve(dirName, pathFile);
+        const dirFullPath = path.resolve(dirName);
+        if (!(await fileExists(dirFullPath))) {
+            await createFolderAsync(dirFullPath);
+        }
         await writeFileAsync(pathFileFull, '');
     } catch (error) {
-        console.log(error);
+        console.log("Error --->>> ", error);
     }
 };
 
---
+++ utils/deleteFile.js
@@ -0,0 +1,16 @@
+const { rm, access } = require('fs/promises');
+const { fileExists } = require('./existsFile');
+
+async function rmFileAsync(pathFile) {
+    try {
+        if (await fileExists(pathFile)) {
+            await rm(pathFile);
+        } else {
+            console.log("file don't exists");
+        }
+    } catch (error) {
+        console.log("Error --->>> ", error);
+    }
+}
+
+module.exports = { rmFileAsync };
